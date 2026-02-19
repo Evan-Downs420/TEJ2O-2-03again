e0e5ef52a28b8865fdb993a8837ab1a9dd9ff193
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-02-19 15:31:31 -0500

    change

--- main.ts
+++ main.ts
@@ -8,15 +8,13 @@
 basic.clearScreen();
 basic.pause(1000);
 
-// Display dimensions
+// Correct Output per instructions
 basic.showString("A rectangle has dimensions 5 cm & 3 cm.");
 
-// Calculate and display perimeter: P = 2 * (L + W)
 basic.showString("The perimeter would be: ");
 basic.showNumber(2 * (5 + 3));
 basic.showString(" cm.");
 
-// Calculate and display area: A = L * W
 basic.showString("The area would be: ");
 basic.showNumber(5 * 3);
 basic.showString(" cm^2.");
