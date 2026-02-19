a898efe11e0870efcf059c0db70457d38979b6fe
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-02-19 15:26:02 -0500

    changed everything

--- main.ts
+++ main.ts
@@ -1,18 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan
+ * Created on: Feb 2020
+ * This program will do basic math
 */
-
-let perimeter = 2 * (5 + 3);
-let area = 5 * 3;
 
 basic.clearScreen();
 basic.pause(1000);
-basic.showString("A rectangle has dimensions 5cm & 3cm");
-basic.showString("The perimeter would be");
-basic.showNumber(perimeter);
-basic.showString("the area would be");
-basic.showNumber(area);
-basic.showString("cm");
+
+// Display dimensions
+basic.showString("A rectangle has dimensions 5 cm & 3 cm.");
+
+// Calculate and display perimeter: P = 2 * (L + W)
+basic.showString("The perimeter would be: ");
+basic.showNumber(2 * (5 + 3));
+basic.showString(" cm.");
+
+// Calculate and display area: A = L * W
+basic.showString("The area would be: ");
+basic.showNumber(5 * 3);
+basic.showString(" cm^2.");
