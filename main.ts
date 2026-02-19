12092163638c2ebd2a98b7a1492b44ff84396566
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-02-19 15:03:20 -0500

    changed i;sgekjhgjhfv

--- main.ts
+++ main.ts
@@ -5,4 +5,11 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen();
+basic.pause(1000);
+basic.showString("A rectangle has dimensions 5cm & 3cm");
+basic.showString("The perimeter would be");
+basic.showString((2 * (5 + 3)).toString());
+basic.showString("the area would be");
+basic.showString((5 * 3).toString());
+basic.showString("cm");
